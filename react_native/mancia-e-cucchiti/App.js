74680d9c5fbd2d3dc12e6edb504220f2e7cd544c
Emanuele Moro <146356995+hikerema@users.noreply.github.com>
2024-11-22 22:34:02 +0100

    aggiunte schermate base

--- react_native/mancia-e-cucchiti/App.js
+++ react_native/mancia-e-cucchiti/App.js
@@ -1,11 +1,66 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import StorageManager from './services/StorageManager.js';
+import Menu from './screens/Menu.js';
+import Profile from './screens/Profile.js';
+import OrderState from './screens/OrderState.js';
+import Orders from './screens/Orders.js';
+import Details from './screens/Details.js';
+
+import { useState } from 'react';
+
 
 export default function App() {
+  const [screen, setScreen] = useState('menu');
+
+  const val = Math.floor(Math.random() * 5);
+    if (val === 0) {
+      setScreen('menu');
+    }
+    if (val === 1) {
+      setScreen('profile');
+    }
+    if (val === 2) {
+      setScreen('orderstate');
+    }
+    if (val === 3) {
+      setScreen('orders');
+    }
+    if (val === 4) {
+      setScreen('details');
+    }
+  if (screen === 'menu') {
+    return (
+      <Menu />
+    ); 
+  }
+  if (screen === 'profile') {
+    return (
+      <Profile />
+    ); 
+  }
+  if (screen === 'orderstate') {
+    return (
+      <OrderState />
+    ); 
+  }
+  if (screen === 'orders') {
+    return (
+      <Orders />
+    ); 
+  }
+  if (screen === 'details') {
+    return (
+      <Details />
+    ); 
+  }
+  
+
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
     </View>
   );
 }
